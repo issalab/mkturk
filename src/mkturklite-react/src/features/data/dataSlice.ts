e28eb329f865e5a44d3b15e6efed90029b79c053
hectorcho <yc3135@columbia.edu>
2022-04-13 17:58:49 -0400

    async thunk for checkFileStatus

--- src/mkturklite-react/src/features/data/dataSlice.ts
+++ src/mkturklite-react/src/features/data/dataSlice.ts
@@ -41,6 +41,13 @@ export const loadDatafileAsync = createAsyncThunk(
   }
 );
 
+export const checkFileStatusAsync = createAsyncThunk(
+  'storate/checkFileStatus',
+  async (entry: DataFileEntry) => {
+    return;
+  }
+);
+
 export const dataSlice = createSlice({
   name: 'storage',
   initialState,
