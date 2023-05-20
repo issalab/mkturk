ea0912ecc16cf4ae0e4169fe68b16712be6a3be3
Elias Issa <elias.issa@columbia.edu>
2023-05-20 09:13:35 -0400

    mkfiles: update display of params in task json
    Params now sort & color by groups of default, task, other, automator, and nuisance. The other group is the largest. Parameters are now listed by alphabetical order within their group.
    
    Also, fixed issue where shortcuts at top would double load. This was because of listing files in root before listing files in subfolder. Now avoid this by temporarily making root point to the subfolder and then resetting it to main mkturkfiles folder.

--- src/mkfiles/src/editor-params.ts
+++ src/mkfiles/src/editor-params.ts
@@ -1,53 +1,15 @@
 export const taskParamSchema = {
   title: 'MkTurk Task Params',
   description: 'Object containing MkTurk task parameters',
-  type: ['object', 'array'],
+  type: ['object', 'array','string'],
   properties: {
     Agent: {
       type: 'string',
+      examples: ['SaveImages'],
     },
-    CheckRFID: {
-      type: 'integer',
-    },
-
-    Automator: {
-      type: ['string', 'integer'],
-    },
-    AutomatorFilePath: {
+    Species: {
       type: 'string',
-      examples: [
-        '/mkturkfiles/paramterfiles/automators/touch_m2s_objectome.json',
-      ],
-    },
-    CurrentAutomatorStage: {
-      type: 'integer',
-    },
-    MinPercentCriterion: {
-      type: 'integer',
-    },
-    MinTrialsCriterion: {
-      type: 'integer',
-    },
-    DragtoRespond: {
-      type: 'integer',
-    },
-    CalibrateEye: {
-      type: 'integer',
-    },
-    NRSVP: {
-      type: 'integer',
-    },
-    SameDifferent: {
-      type: 'integer',
-    },
-    SamplingStrategy: {
-      type: 'string',
-    },
-    NStickyResponse: {
-      type: 'integer',
-    },
-    NTrialsPerBagBlock: {
-      type: 'integer',
+      examples: ['human', 'marmoset', 'model'],
     },
     NGridPoints: {
       type: 'integer',
@@ -56,26 +18,101 @@ export const taskParamSchema = {
     GridSpacingInches: {
       type: 'number',
     },
-    GridXOffsetInches: {
-      type: 'number',
-    },
-    GridYOffsetInches: {
-      type: 'number',
-    },
     FixationGridIndex: {
       type: 'integer',
       minimum: -1,
     },
-    SampleGridIndex: {
-      type: 'integer',
-      minimum: -1,
+    FixationSizeInches: {
+      type: 'number',
     },
-    ObjectGridIndex: {
+    RewardDuration: {
+      type: 'integer',
+      minimum: 0,
+      examples: ['in milliseconds'],
+    },
+    PunishTimeOut: {
+      type: 'integer',
+      minimum: 0,
+      examples: ['in milliseconds'],
+    },
+    RewardStage: {
+      type: 'integer',
+      default: 0,
+      examples: ['0 (fixation)','1 (sample or choice)'],
+    },
+    NRSVP: {
+      type: 'integer',
+      default: 0,
+      examples: ['# of movie clips to show in rsvp sequence'],
+    },
+    SameDifferent: {
+      type: 'integer',
+      default: 0,
+    },
+    VisualSearch: {
+      type: 'integer',
+      default: 0,
+      examples: ['0 (random choice from test bag)','1 (select test image with same index as sample image)'],
+    },
+    ImageBagsSample: {
       type: 'array',
+      default: '/mkturkfiles/scenebags/objectome3d/camel/20200709_camel_token.js, /mkturkfiles/scenebags/objectome3d/wrench/20200709_wrench_token.js',
       items: {
-        type: 'integer',
-        minimum: 0,
+        type: 'string',
       },
+    },
+    ImageBagsTest: {
+      type: 'array',
+      default: '/mkturkfiles/scenebags/objectome3d/camel/20200709_camel_token.js, /mkturkfiles/scenebags/objectome3d/wrench/20200709_wrench_token.js',
+      items: {
+        type: 'string',
+      },
+    },
+    SamplingStrategy: {
+      type: 'string',
+      default: 'uniform_with_replacement',
+      examples: ['uniform_with_replacement','uniform_without_replacement','sequential'],
+    },
+    Target:{
+      type: 'string',
+      default: 'object',
+      examples: ['gridwindow','objectwindow','object (default)','scene'],
+    },
+    BackgroundColor2D: {
+      type: 'string',
+      default: '#7F7F7F',
+      examples: ['#000000 (black)','#FFFFFF (white)'],
+    },
+    BlinkGracePeriod:{
+      type: 'integer',
+      default: '200',
+      examples: ['in milliseconds'],
+    },
+    BQSaveDisplayTimes: {
+      type: 'integer',
+      default: '1',
+    },
+    BQSaveEye: {
+      type: 'integer',
+      default: '1',
+    },
+    BQSaveTouch: {
+      type: 'integer',
+      default: '1',
+    },
+    CalibrateEye: {
+      type: 'integer',
+      default: '0',
+      examples: ['0 (do not calibrate)','n (calibrate after n trials)'],
+    },
+    CalibrateEyeCrossTerms: {
+      type: 'integer',
+      default: '0',
+      examples: ['0 (do not calibrate with crossterms)','>0 (calibrate with crossterms)'],
+    },
+    CheckRFID: {
+      type: 'integer',
+      default: '0',
     },
     ChoiceGridIndex: {
       type: 'array',
@@ -83,6 +120,168 @@ export const taskParamSchema = {
         type: 'integer',
         minimum: 0,
       },
+      default: '[0,1]',
+      examples: ['[m,n] --> [location of same circle, location of different square]'],
+    },
+    ChoiceOutsideGracePeriod: {
+      type: 'integer',
+      default: 'TASK.ChoiceTimeOut+1',
+      examples: ['>TASK.ChoiceTimeOut (do not punish outside target)'],
+    },
+    ChoiceSizeInches: {
+      type: 'number',
+      default: '1',
+    },
+    ChoiceTimeOut: {
+      type: 'integer',
+      default: '5000',
+    },
+    DeviceConfig: {
+      type: 'string',
+      default: 'empty string',
+    },
+    FixationDotSizeInches: {
+      type: 'number',
+      default: '0',
+      examples: ['>0 (overlays white fixation square'],
+    },
+    FixationDuration: {
+      type: 'integer',
+      minimum: 0,
+      examples: ['in milliseconds (duration of fixation hold)'],
+    },
+    FixationOutsideGracePeriod: {
+      type: 'integer',
+      default: '0',
+      examples: ['0 (punish outside fixation dot'],
+    },
+    FixationTimeOut: {
+      type: 'integer',
+      default: '3000',
+      examples: ['in milliseconds'],
+    },
+    FixationUsesSample: {
+      type: 'integer',
+      default: '0',
+      examples: ['0 (blue fixation circle is shown','1 (sample image is shown)'],
+    },
+    FixationWindowSizeInches: {
+      type: 'number',
+      default: '0',
+    },
+    GridXOffsetInches: {
+      type: 'number',
+      default: '0',
+    },
+    GridYOffsetInches: {
+      type: 'number',
+      default: '0',
+    },
+    HeadsupDisplayFraction: {
+      type: 'number',
+      default: 0,
+      examples: ['places black region with text at top of screen'],
+    },
+    HideChoiceDistractors: {
+      type: 'integer',
+      default: '0',
+      examples: ['1 (hides the same or different button so that only correct option is available)'],
+    },
+    HideTestDistractors: {
+      type: 'integer',
+      default: '0',
+      examples: ['1 (hides the distractor choices so that only correct option is available)'],
+    },
+    InterTrialInterval: {
+      type: 'integer',
+      minimum: 0,
+      default: '0',
+      examples: ['in milliseconds'],
+    },
+    KeepSampleON: {
+      type: 'integer',
+      default: '0',
+      examples: ['0 (extinguish sample before test/choice), 1 (sample stays on for test/choice)'],
+    },
+    KeepTestON: {
+      type: 'integer',
+      default: '0',
+      examples: ['0 (test only present for testON ms), 1 (test remains for choice screen)'],
+    },
+    MinTrialDuration_AfterSampleCommandTrigger:{
+      type: 'integer',
+      default: '-1',
+    },
+    NConsecutiveHitsforBonus: {
+      type: 'integer',
+      default: 0,
+    },
+    NFixations: {
+      type: 'integer',
+      default: 1,
+      examples: ['n (subject has to press fixation n times to initiate sample presentation'],
+    },
+    NMillisecondsPerBagBlock:{
+      type: 'integer',
+      default: '0',
+      examples: ['overrides NStimuliPerBagBlock'],
+    },
+    NRewardMax: {
+      type: 'integer',
+      default: 1,
+      examples: ['1 (no bonus reward possible)','n (max rewards possible per trial)']
+    },
+    NRSVPMax: {
+      type: 'integer',
+      default: 0,
+      examples: ['0 (no exponential reward for fixating NRSVP seq)','>NRSVP (exponentially more reward pulses after NRSVP, 1 pulse for NRSVP)'],
+    },
+    NStickyResponse: {
+      type: 'integer',
+      default: 0,
+      examples: ['0 (subject can repeatedly choose a given response over trials','n (after n, the neglected choice is set as correct until selected)'],
+    },
+    NStimuliPerBagBlock: {
+      type: 'integer',
+      default: '0',
+    },
+    ObjectGridIndex: {
+      type: 'array',
+      items: {
+        type: 'integer',
+        minimum: 0,
+      },
+      default: '[]',
+      examples: ['array of location of each object class in ImageBagsTest array'],
+    },
+    Photodiode: {
+      type: 'integer',
+      default: 1,
+    },
+    SampleGridIndex: {
+      type: 'integer',
+      default: '=FixationGridIndex',
+      examples: ['<0 (sample appears at a randomly selected grid location)'],
+    },
+    SampleOFF: {
+      type: 'integer',
+      default: '0',
+      examples: ['in milliseconds'],
+    },
+    SampleOutsideGracePeriod: {
+      type: 'integer',
+      default: '0',
+      examples: ['0 (punish outside sample targets', '>0 milliseconds (after sample onset, time allowed to view any location without punish'],
+    },
+    SamplePRE: {
+      type: 'integer',
+      default: '0',
+      examples: ['in milliseconds'],
+    },
+    SaveImagesResolution: {
+      type: 'integer',
+      default: '0',
+      examples: ['0 (full 3js window)','N (N x N pixels)'],
     },
     TestGridIndex: {
       type: 'array',
@@ -90,114 +289,51 @@ export const taskParamSchema = {
         type: 'integer',
         minimum: 0,
       },
-    },
-    NFixations: {
-      type: 'integer',
-      minimum: 1,
-    },
-    FixationUsesSample: {
-      type: 'integer',
-    },
-    FixationSizeInches: {
-      type: 'number',
-    },
-    FixationDuration: {
-      type: 'integer',
-    },
-    FixationTimeOut: {
-      type: 'integer',
-    },
-    FixationWindowSizeInches: {
-      type: 'number',
-    },
-    FixationDotSizeInches: {
-      type: 'number',
-    },
-    ImageBagsSample: {
-      type: 'array',
-      items: {
-        type: 'string',
-      },
-    },
-    KeepSampleON: {
-      type: 'integer',
-    },
-    SamplePRE: {
-      type: 'integer',
-      minimum: 0,
-    },
-    SampleOFF: {
-      type: 'integer',
-      minimum: 0,
-    },
-
-    ImageBagsTest: {
-      type: 'array',
-      items: {
-        type: 'string',
-      },
-    },
-    KeepTestON: {
-      type: 'integer',
+      default: '[0,1]',
+      examples: ['length n array (N-way task)'],
     },
     TestOFF: {
       type: 'integer',
+      default: '0',
+      examples: ['in milliseconds'],
     },
-    HideTestDistractors: {
+    THREEJSCameraFOV: {
       type: 'integer',
+      default: '45',
+      examples: ['degrees'],
     },
-
-    ChoiceSizeInches: {
-      type: 'number',
-      minimum: 0,
-    },
-    HideChoiceDistractors: {
+    THREEJSCameraZDist: {
       type: 'integer',
+      default: '10',
+      examples: ['in 3js units'],
     },
-    ChoiceTimeOut: {
+    THREEJSRenderRatio: {
       type: 'integer',
-      minimum: 0,
+      default: '2',
+      examples: ['how much to upsample for rendering to 2D canvas'],
     },
-
-    RewardStage: {
-      type: 'integer',
+    Automator: {
+      type: ['string', 'integer'],
+      default: '0',
     },
-    RewardPer1000Trials: {
-      type: 'integer',
-    },
-    NRewardMax: {
-      type: 'integer',
-    },
-    NConsecutiveHitsforBonus: {
-      type: 'integer',
-    },
-    PunishTimeOut: {
-      type: 'integer',
-      minimum: 0,
-    },
-    ConsecutiveHitsITI: {
-      type: 'integer',
-      minimum: 0,
-    },
-    InterTrialInterval: {
-      type: 'integer',
-      minimum: 0,
-    },
-    BackgroundColor2D: {
+    AutomatorFilePath: {
       type: 'string',
+      default: '',
+      examples: [
+        '/mkturkfiles/paramterfiles/automators/touch_m2s_objectome.json',
+      ],
     },
-    Homecage: {
+    CurrentAutomatorStage: {
       type: 'integer',
+      default: '0',
     },
-    Separated: {
+    MinPercentCriterion: {
       type: 'integer',
+      default: '0',
     },
-    Species: {
-      type: 'string',
-      examples: ['human', 'marmoset', 'model'],
-    },
-    Liquid: {
+    MinTrialsCriterion: {
       type: 'integer',
+      default: '0',
     },
   },
 };
--- src/mkfiles/src/mkmedia.ts
+++ src/mkfiles/src/mkmedia.ts
@@ -213,7 +213,67 @@ export class Mkeditor {
         `onClassName path=${classNameParams.path}, field=${classNameParams.field}, value=${classNameParams.value}`
       );
 
-      const bioKeys = ['Agent', 'CheckRFID'];
+      const defaultKeys = [
+        'Agent', 'Species',
+        'NGridPoints','GridSpacingInches',
+        'FixationGridIndex','FixationSizeInches',
+        'RewardDuration','PunishTimeOut',
+      ];
+      const taskKeys = [
+        'RewardStage','NRSVP','SameDifferent','VisualSearch',
+        'ImageBagsSample','ImageBagsTest',
+        'SamplingStrategy','Target',
+      ];
+      const otherKeys = [
+        'BackgroundColor2D',
+        'BlinkGracePeriod',
+        'BQSaveDisplayTimes',
+        'BQSaveEye',
+        'BQSaveTouch',
+        'CalibrateEye',
+        'CalibrateEyeCrossTerms',
+        'CheckRFID',
+        'ChoiceGridIndex',
+        'ChoiceOutsideGracePeriod',
+        'ChoiceSizeInches',
+        'ChoiceTimeOut',
+        'DeviceConfig',
+        'FixationDotSizeInches',
+        'FixationDuration',
+        'FixationOutsideGracePeriod',
+        'FixationTimeOut',
+        'FixationUsesSample',
+        'FixationWindowSizeInches',
+        'GridXOffsetInches',
+        'GridYOffsetInches',
+        'HeadsupDisplayFraction',
+        'HideChoiceDistractors',
+        'HideTestDistractors',
+        'InterTrialInterval',
+        'KeepSampleON',
+        'KeepTestON',
+        'MinTrialDuration_AfterSampleCommandTrigger',
+        'NConsecutiveHitsforBonus',
+        'NFixations',
+        'NMillisecondsPerBagBlock',
+        'NRewardMax',
+        'NRSVPMax',
+        'NStickyResponse',
+        'NStimuliPerBagBlock',
+        'ObjectGridIndex',
+        'Photodiode',
+        'SampleGridIndex',
+        'SampleOFF',
+        'SampleOutsideGracePeriod',
+        'SamplePRE',
+        'SaveImagesResolution',
+        'TestGridIndex',
+        'TestOFF',
+        'THREEJSCameraFOV',
+        'THREEJSCameraZDist',
+        'THREEJSRenderRatio',
+
+      ]
       const automatorKeys = [
         'Automator',
         'AutomatorFilePath',
@@ -221,85 +281,89 @@ export class Mkeditor {
         'MinPercentCriterion',
         'MinTrialsCriterion',
       ];
-      const generalKeys = [
-        'DragtoRespond',
-        'CalibrateEye',
-        'NRSVP',
-        'NRSVPMax',
-        'SameDifferent',
-        'SamplingStrategy',
-        'NStickyResponse',
-        'NTrialsPerBagBlock',
-      ];
-      const gridKeys = [
-        'NGridPoints',
-        'GridSpacingInches',
-        'GridXOffsetInches',
-        'GridYOffsetInches',
-        'FixationGridIndex',
-        'SampleGridIndex',
-        'ObjectGridIndex',
-        'ChoiceGridIndex',
-        'TestGridIndex',
-      ];
-      const fixationKeys = [
-        'NFixations',
-        'FixationUsesSample',
-        'FixationSizeInches',
-        'FixationDuration',
-        'FixationTimeOut',
-      ];
-      const fixationConfigKeys = [
-        'FixationWindowSizeInches',
-        'FixationDotSizeInches',
-      ];
-      const sampleKeys = [
-        'ImageBagsSample',
-        'KeepSampleON',
-        'SamplePRE',
-        'SampleOFF',
-      ];
-      const testKeys = [
-        'ImageBagsTest',
-        'KeepTestON',
-        'TestOFF',
-        'HideTestDistractors',
-      ];
-      const choiceKeys = [
-        'ChoiceSizeInches',
-        'HideChoiceDistractors',
-        'ChoiceTimeOut',
-      ];
-      const rewardKeys = [
-        'RewardStage',
-        'RewardPer1000Trials',
-        'NRewardMax',
-        'NConsecutiveHitsforBonus',
-        'PunishTimeOut',
-        'ConsecutiveHitsITI',
-      ];
+      // const bioKeys = ['CheckRFID'];
+      // const generalKeys = [
+      //   'DragtoRespond',
+      //   'CalibrateEye',
+      //   'NRSVP',
+      //   'NRSVPMax',
+      //   'SameDifferent',
+      //   'SamplingStrategy',
+      //   'NStickyResponse',
+      //   'NTrialsPerBagBlock',
+      // ];
+      // const gridKeys = [
+      //   'NGridPoints',
+      //   'GridSpacingInches',
+      //   'GridXOffsetInches',
+      //   'GridYOffsetInches',
+      //   'SampleGridIndex',
+      //   'ObjectGridIndex',
+      //   'ChoiceGridIndex',
+      //   'TestGridIndex',
+      // ];
+      // const fixationKeys = [
+      //   'NFixations',
+      //   'FixationUsesSample',
+      //   'FixationTimeOut',
+      // ];
+      // const fixationConfigKeys = [
+      //   'FixationWindowSizeInches',
+      //   'FixationDotSizeInches',
+      // ];
+      // const sampleKeys = [
+      //   'ImageBagsSample',
+      //   'KeepSampleON',
+      //   'SamplePRE',
+      //   'SampleOFF',
+      // ];
+      // const testKeys = [
+      //   'ImageBagsTest',
+      //   'KeepTestON',
+      //   'TestOFF',
+      //   'HideTestDistractors',
+      // ];
+      // const choiceKeys = [
+      //   'ChoiceSizeInches',
+      //   'HideChoiceDistractors',
+      //   'ChoiceTimeOut',
+      // ];
+      // const rewardKeys = [
+      //   'RewardStage',
+      //   'NRewardMax',
+      //   'NConsecutiveHitsforBonus',
+      //   'ConsecutiveHitsITI',
+      // ];
 
-      if (bioKeys.includes(classNameParams.field)) {
-        return 'color-node-bio';
+      if (defaultKeys.includes(classNameParams.field)) {
+        return 'color-node-default';
+      } else if (taskKeys.includes(classNameParams.field)) {
+        return 'color-node-task';
+      } else if (otherKeys.includes(classNameParams.field)) {
+        return 'color-node-other';
       } else if (automatorKeys.includes(classNameParams.field)) {
         return 'color-node-automator';
-      } else if (generalKeys.includes(classNameParams.field)) {
-        return 'color-node-general';
-      } else if (gridKeys.includes(classNameParams.field)) {
-        return 'color-node-grid';
-      } else if (fixationKeys.includes(classNameParams.field)) {
-        return 'color-node-fixation';
-      } else if (fixationConfigKeys.includes(classNameParams.field)) {
-        return 'color-node-fixation-config';
-      } else if (sampleKeys.includes(classNameParams.field)) {
-        return 'color-node-sample';
-      } else if (testKeys.includes(classNameParams.field)) {
-        return 'color-node-test';
-      } else if (choiceKeys.includes(classNameParams.field)) {
-        return 'color-node-choice';
-      } else if (rewardKeys.includes(classNameParams.field)) {
-        return 'color-node-reward';
-      } else {
+      }
+      // else if (bioKeys.includes(classNameParams.field)) {
+      //   return 'color-node-bio';
+      // } else if (generalKeys.includes(classNameParams.field)) {
+      //   return 'color-node-general';
+      // } else if (gridKeys.includes(classNameParams.field)) {
+      //   return 'color-node-grid';
+      // } else if (fixationKeys.includes(classNameParams.field)) {
+      //   return 'color-node-fixation';
+      // } else if (fixationConfigKeys.includes(classNameParams.field)) {
+      //   return 'color-node-fixation-config';
+      // } else if (sampleKeys.includes(classNameParams.field)) {
+      //   return 'color-node-sample';
+      // } else if (testKeys.includes(classNameParams.field)) {
+      //   return 'color-node-test';
+      // } else if (choiceKeys.includes(classNameParams.field)) {
+      //   return 'color-node-choice';
+      // } else if (rewardKeys.includes(classNameParams.field)) {
+      //   return 'color-node-reward';
+      // }
+      else {
         return 'color-node-nuisance';
       }
     }
